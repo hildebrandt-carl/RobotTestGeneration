b9f2d3ca03f5b585a8cb7ec3d243a13b06991888
Carl Hildebrandt <hildebrandt.carl@outlook.com>
2019-06-22 13:29:31 -0400

    Added updated to the python scalling, updating unity to have a larger base

--- TestGen/ConversionUtils.py
+++ TestGen/ConversionUtils.py
@@ -80,20 +80,30 @@ def pythonMaptoWalls(map, distance_threshold):
 def wallsToUnityFile(walls, waypoints, savename="default", raining=False, day=True):
 	file = open(savename + ".txt", "w")
 
+	file.write('# Note +ve X is north\n')
+	file.write('# Note +ve Y is West\n')
+	file.write('# x, y, height\n')
 	file.write("# waypoints\n")
 	# Go through each waypoint
 	for waypoint in waypoints:
 		file.write("G: (" + str(waypoint[0] + 10) + ',' + str(waypoint[1] - 25) + ",15)\n")
 
 	file.write("\n\n# wall segments\n")
+	file.write('# x1, y1, x2, y2, z\n')
+	# Python map is 25 x 25
+	# Unity map is 100 * 100
+	x_shift = 0
+	y_shift = 13.5
+	x_scale = 2
+	y_scale = 2
 	# For each segment of walls
 	for wallsegment in walls:
 		# For each wall
 		for wall in wallsegment:
-			x_start = str(wall[0][1] + 10)
-			y_start = str(wall[0][0] - 25)
-			x_end = str(wall[1][1] + 10)
-			y_end = str(wall[1][0] - 25)
+			x_start = str((wall[0][1] - x_shift) * x_scale)
+			y_start = str((wall[0][0] - y_shift) * y_scale)
+			x_end = str((wall[1][1] - x_shift) * x_scale)
+			y_end = str((wall[1][0] - y_shift) * y_scale)
 			file.write("W: (" + x_start + "," + y_start + "," + x_end + "," + y_end + ",3)\n")
 
 	file.write("\n\n# environment settings\n")
@@ -101,3 +111,11 @@ def wallsToUnityFile(walls, waypoints, savename="default", raining=False, day=Tr
 	file.write("D: (" + str(int(day)) + ")\n")
 
 	file.close()
+
+
+# Note +ve X is north
+# Note +ve Y is West
+# x1, y1, x2, y2, z
+W: (-10,50,90,50,3)
+
+# x, y, height
