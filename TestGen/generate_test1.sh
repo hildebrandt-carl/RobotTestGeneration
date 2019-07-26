da90c7938392813d47420be83410bddf92123c3c
Carl Hildebrandt <hildebrandt.carl@outlook.com>
2019-07-26 15:30:25 -0400

    Changed the amount of time each test has to 12 hours

--- TestGen/generate_test1.sh
+++ TestGen/generate_test1.sh
@@ -7,7 +7,7 @@ do
     do
         echo Starting Depth $way Drop $drop Nodes $node
         converted=$((100 * $drop))
-        python3 -u resmonitor.py -M 10g -T 86400 time python3 -u main.py --depth $way --drop $drop --nodes $node 2>&1 | tee Results/details\_depth$way\_nodes$node\_drop$converted.txt
+        python3 -u resmonitor.py -M 10g -T 43200 time python3 -u main.py --depth $way --drop $drop --nodes $node 2>&1 | tee Results/details\_depth$way\_nodes$node\_drop$converted.txt
         echo Complete
         echo -----------------------------------------------------------------------------------
     done
