fa6bd622b7b9274c702a7841efa96b380aedac92
Carl Hildebrandt <hildebrandt.carl@outlook.com>
2019-07-26 15:22:16 -0400

    Changed the output of the runs to be save to files

--- TestGen/generate_test1.sh
+++ TestGen/generate_test1.sh
@@ -1,11 +1,13 @@
 #!/bin/zsh
 
+node=500
 for drop in 0.1 0.3
 do
     for way in 3 4 5 6 7
     do
-        echo Starting Depth $way Drop $drop Nodes 500
-        python3 resmonitor.py -M 10g -T 86400 time python3 main.py --depth $way --drop $drop --nodes 500
+        echo Starting Depth $way Drop $drop Nodes $node
+        converted=$((100 * $drop))
+        python3 -u resmonitor.py -M 10g -T 86400 time python3 -u main.py --depth $way --drop $drop --nodes $node 2>&1 | tee Results/details\_depth$way\_nodes$node\_drop$converted.txt
         echo Complete
         echo -----------------------------------------------------------------------------------
     done
