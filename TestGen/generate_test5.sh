8059380776e824413a35c2ae4f450e8d3f75a345
Carl Hildebrandt <hildebrandt.carl@outlook.com>
2019-07-26 15:03:01 -0400

    Added the time command to the scripts

--- TestGen/generate_test5.sh
+++ TestGen/generate_test5.sh
@@ -5,7 +5,7 @@ do
     for way in 10
     do
         echo Starting Depth $way Drop $drop Nodes 500
-        python3 resmonitor.py -M 10g -T 86400 python3 main.py --depth $way --drop $drop --nodes 500
+        python3 resmonitor.py -M 10g -T 86400 time python3 main.py --depth $way --drop $drop --nodes 500
         echo Complete
         echo -----------------------------------------------------------------------------------
     done
