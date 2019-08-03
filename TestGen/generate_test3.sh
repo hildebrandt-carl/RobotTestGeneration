e5b1cbb49bca4429169e0a68ba5cd98fc468ba43
Carl Hildebrandt <hildebrandt.carl@outlook.com>
2019-08-03 13:27:55 -0400

    Updated the scripts to run using unbuffered mode so that it prints the output out

--- TestGen/generate_test3.sh
+++ TestGen/generate_test3.sh
@@ -7,7 +7,7 @@ do
     for depth in 3 4 5 6 7 8 9 10
     do
         echo Starting Depth $depth Beamwidth $beamwidth Nodes $nodes Resolution $res
-        python3 -u resmonitor.py -M 10g -T 43200 python3 main.py --drone bebop --depth $depth --beamwidth $beamwidth --nodes $nodes --resolution $res 2>&1 | tee Results/details\_nodes$nodes\_res$res\_beamwidth$beamwidth.txt
+        python3 -u resmonitor.py -M 10g -T 43200 python3 -u main.py --drone bebop --depth $depth --beamwidth $beamwidth --nodes $nodes --resolution $res 2>&1 | tee Results/details\_nodes$nodes\_res$res\_beamwidth$beamwidth.txt
         echo Complete
         echo -----------------------------------------------------------------------------------
     done
--- TestGen/generate_test6.sh
+++ TestGen/generate_test6.sh
@@ -2,12 +2,12 @@
 
 res=4
 nodes=500
-for beamwidth in 1 2 3 4 5
+for beamwidth in 10
 do
     for depth in 3 4 5 6 7 8 9 10
     do
         echo Starting Depth $depth Beamwidth $beamwidth Nodes $nodes Resolution $res
-        python3 -u resmonitor.py -M 10g -T 43200 python3 main.py --drone bebop --depth $depth --beamwidth $beamwidth --nodes $nodes --resolution $res 2>&1 | tee Results/details\_nodes$nodes\_res$res\_beamwidth$beamwidth.txt
+        python3 -u resmonitor.py -M 10g -T 43200 python3 -u main.py --drone bebop --depth $depth --beamwidth $beamwidth --nodes $nodes --resolution $res 2>&1 | tee Results/details\_nodes$nodes\_res$res\_beamwidth$beamwidth.txt
         echo Complete
         echo -----------------------------------------------------------------------------------
     done
